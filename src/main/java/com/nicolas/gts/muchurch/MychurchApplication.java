71a23f5e7f9b0a52048aebfcca5cf4eff83d7392
Nicolas Gabriel <nicolasgabrielt@gmail.com>
2018-08-03 11:45:02 -0300

    first commit

---
+++ src/main/java/com/nicolas/gts/muchurch/MychurchApplication.java
@@ -0,0 +1,12 @@
+package com.nicolas.gts.muchurch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MychurchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MychurchApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nicolas/gts/muchurch/MychurchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nicolas.gts.muchurch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MychurchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
