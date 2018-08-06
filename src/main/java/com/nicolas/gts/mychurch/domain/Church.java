ea51054aece922f34a9f084d72652ed1f67d4662
Nicolas Gabriel <nicolasgabrielt@gmail.com>
2018-08-06 00:23:47 -0300

    testing the first domain class

---
+++ src/main/java/com/nicolas/gts/mychurch/domain/Church.java
@@ -0,0 +1,90 @@
+package com.nicolas.gts.mychurch.domain;
+
+import java.io.Serializable;
+
+public class Church implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	private String agenda;
+	private String description;
+	//private Contato contato;
+	//private endereco endereco;
+	//private Midia midia; 
+	
+	public Church() {
+		
+	}
+
+	public Church(Integer id, String name, String agenda, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.agenda = agenda;
+		this.description = description;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Church other = (Church) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAgenda() {
+		return agenda;
+	}
+
+	public void setAgenda(String agenda) {
+		this.agenda = agenda;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
--- src/main/java/com/nicolas/gts/mychurch/resources/ChurchResourse.java
+++ src/main/java/com/nicolas/gts/mychurch/resources/ChurchResourse.java
@@ -1,16 +1,33 @@
 package com.nicolas.gts.mychurch.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nicolas.gts.mychurch.domain.Church;
 
 @RestController
 @RequestMapping(value="/churches")
 public class ChurchResourse {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listChurches() {
-		return "Rest is working";
+	public List<Church> listChurches() {
+		
+	
+		
+		Church church1 = new Church(1,"Igreja o Brasil Para Cristo", "Terça: 19:30 - Culto de Oração; Domingo: 18:00 - Culto de Celebração","Igreja evangelica");
+		Church church2 = new Church(2,"Igreja Metodista", "Terça: 19:30 - Culto de Ação de Graças; Domingo: 20:00 - Culto de Celebração","A Igreja Metodista cumpre a sua missão "
+				+ "realizando o culto de Deus, pregando a Sua Palavra, ministrando os sacramentos, promovendo a fraternidade e a disciplina cristãs e proporcionando a seus membros meios "
+				+ "para alcançarem uma experiência cristã progressiva, visando ao desempenho de seu testemunho e serviço no mundo.");
+		
+		List<Church> listOfChurchs =  new ArrayList<>();
+		listOfChurchs.add(church1);
+		listOfChurchs.add(church2);
+		
+		return listOfChurchs;
 	}
 
 }
