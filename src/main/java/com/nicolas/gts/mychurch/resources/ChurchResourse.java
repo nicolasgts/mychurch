d8f1233951dd0048ddd19cce8e625d079cbf4f27
Nicolas Gabriel <nicolasgabrielt@gmail.com>
2018-08-06 16:35:22 -0300

    Endpoint to insert a new church

--- src/main/java/com/nicolas/gts/mychurch/resources/ChurchResourse.java
+++ src/main/java/com/nicolas/gts/mychurch/resources/ChurchResourse.java
@@ -1,13 +1,16 @@
 package com.nicolas.gts.mychurch.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nicolas.gts.mychurch.domain.Church;
 import com.nicolas.gts.mychurch.services.ChurchService;
@@ -30,5 +33,14 @@ public class ChurchResourse {
 		List<Church> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Church obj) {
+	obj = service.insert(obj);
+	URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+		.path("/{id}").buildAndExpand(obj.getId()).toUri();
+	return ResponseEntity.created(uri).build();
+}
+	
 
 }
